6021626f4041d08a66be74f9b5ddb24797a3f5f0
mejora45 <admin@mejorainfotech.com>
2025-08-27 12:42:34 +0530

    Update files and pages

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/homework-helper" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
